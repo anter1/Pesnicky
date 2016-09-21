2282cb93aa86c1df2c84ed80d8fcf54d284dfe9d
Pavol Mravec <pavolm@fb.com>
2016-09-21 00:43:13 -0700

    add vcs.xml

--- app/src/main/java/com/example/pavolm/pesnicky/Song.java
+++ app/src/main/java/com/example/pavolm/pesnicky/Song.java
@@ -1,5 +1,6 @@
 package com.example.pavolm.pesnicky;
 
+import android.graphics.Bitmap;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.DrawableRes;
@@ -10,11 +11,20 @@ import android.support.annotation.DrawableRes;
 public class Song implements Parcelable {
     public String name;
     public @DrawableRes int icon;
+    public Bitmap bitmap;
     public String notes;
 
     public Song(String name, @DrawableRes int icon, String notes) {
         this.name = name;
         this.icon = icon;
+        this.notes = notes;
+        this.bitmap = null;
+    }
+
+    public Song(String name, Bitmap bitmap, String notes) {
+        this.name = name;
+        this.icon = 0;
+        this.bitmap = bitmap;
         this.notes = notes;
     }
 
--- app/src/main/java/com/example/pavolm/pesnicky/SongListActivity.java
+++ app/src/main/java/com/example/pavolm/pesnicky/SongListActivity.java
@@ -2,26 +2,41 @@ package com.example.pavolm.pesnicky;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Base64;
+import android.view.*;
 import android.widget.*;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SongListActivity extends AppCompatActivity {
     List<Song> mSongs;
     ListView mSongListView;
+    BaseAdapter mSongListAdapter;
+
+    RequestQueue mRequestQueue;
 
     public SongListActivity() {
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mRequestQueue = Volley.newRequestQueue(this);
         super.onCreate(savedInstanceState);
         mSongs = new ArrayList<>();
         mSongs.add(new Song("Kohútik", R.drawable.kohutik, "CDEFFFFEDEEEEDCDDDDEDCCC"));
@@ -29,12 +44,13 @@ public class SongListActivity extends AppCompatActivity {
         mSongs.add(new Song("Rudolf", R.drawable.rudolf, "GAGEcAGGAGAGcBFGFDBAGGAGAGdc"));
         mSongs.add(new Song("Ide ide vláčik", R.drawable.ide_ide_vlacik, "GGGAGEGFEDCGGGAGEGFEDCFDDDECCCFDDDECCCGGGAGEGFEDC"));
         mSongs.add(new Song("Ide vláčik ši-ši-ši", R.drawable.ide_vlacik_si_si_si, "GGGAGGEGGGAGGEGGGEGGGGEGGEEE"));
-        mSongs.add(new Song("Itsy bitsy spider", R.drawable.itsy_bitsy_spider, "GGGABBBAGABGBBcddcBcdBGGABBAGABADDGGGABBBAGABG"));
+        mSongs.add(new Song("Itsy bitsy spider", R.drawable.itsy_bitsy_spider, "GGGABBBAGABGBBcddcBcdBGGABBAGABGDDGGGABBBAGABG"));
 
         setContentView(R.layout.activity_song_list);
 
+        mSongListAdapter = new SongListAdapter(this);
         mSongListView = (ListView)this.findViewById(R.id.songListView);
-        mSongListView.setAdapter(new SongListAdapter(this));
+        mSongListView.setAdapter(mSongListAdapter);
 
         mSongListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -45,6 +61,59 @@ public class SongListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.song_list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.refresh) {
+            startRefresh();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void startRefresh() {
+        JsonObjectRequest request = new JsonObjectRequest(
+                "http://pesnicky2.appspot.com/json",
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        List<Song> result = new ArrayList<>();
+                        try {
+                            JSONArray songs = response.getJSONArray("songs");
+                            for (int i = 0; i < songs.length(); ++i) {
+                                JSONObject song = songs.getJSONObject(i);
+                                String name = song.getString("name");
+                                String encodedImage = song.getString("image");
+                                String notes = song.getString("notes");
+                                byte[] imageBytes = Base64.decode(encodedImage, Base64.DEFAULT);
+                                Bitmap icon = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+
+                                result.add(new Song(name, icon, notes));
+                            }
+                        } catch (JSONException e) {
+                            // nom nom
+                        }
+
+                        mSongs.clear();
+                        mSongs.addAll(result);
+                        mSongListAdapter.notifyDataSetChanged();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(SongListActivity.this, "Failed to download songs: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+        mRequestQueue.add(request);
     }
 
     public class SongListAdapter extends ArrayAdapter<Song> {
@@ -63,7 +132,11 @@ public class SongListActivity extends AppCompatActivity {
             titleView.setText(song.name);
 
             ImageView iconView = (ImageView) convertView.findViewById(R.id.icon);
-            iconView.setImageResource(song.icon);
+            if (song.bitmap != null) {
+                iconView.setImageBitmap(song.bitmap);
+            } else {
+                iconView.setImageResource(song.icon);
+            }
 
             return convertView;
         }
